42fec2381551bd7d52a6281e4a2689a846f8df22
Akshay N V <akshayvenkatesha@gmail.com>
2018-01-28 15:32:44 +0530

    initial interation of native method. still not working

--- notificationApp/App.js
+++ notificationApp/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Text, View, NativeModules, DeviceEventEmitter } from 'react-native'
+import { Text, View, NativeModules, DeviceEventEmitter, AppRegistry } from 'react-native'
 import { StackNavigator } from 'react-navigation'
 
 import AllNotifications from './screens/AllNotifications'
@@ -22,40 +22,46 @@ const Navigator = StackNavigator({
   }
 });
 
-const eventsMap = {
-  notification: 'notificationReceived',
-  //installedApps: 'installedApps',
-};
-const NotificationModule = NativeModules.NotificationModule;
-const Notification = {};
+// const eventsMap = {
+//   notification: 'notificationReceived',
+//   //installedApps: 'installedApps',
+// };
+// const NotificationModule = NativeModules.NotificationModule;
+// const Notification = {};
 
-Notification.getPermissionStatus = () => {
-  return NotificationModule.getPermissionStatus();
-}
+// Notification.getPermissionStatus = () => {
+//   return NotificationModule.getPermissionStatus();
+// }
 
-Notification.requestPermission = () => {
-  return NotificationModule.requestPermission();
-}
+// Notification.requestPermission = () => {
+//   return NotificationModule.requestPermission();
+// }
 
-Notification.getInstalledApps = () => {
-  return NotificationModule.getInstalledApps();
-}
+// Notification.getInstalledApps = () => {
+//   return NotificationModule.getInstalledApps();
+// }
 
-Notification.on = (event, callback) => {
-  const nativeEvent = eventsMap[event];
-  if (!nativeEvent) {
-    throw new Error('Invalid event');
-  }
-  DeviceEventEmitter.removeAllListeners(nativeEvent);
-  return DeviceEventEmitter.addListener(nativeEvent, callback);
-}
+// Notification.on = (event, callback) => {
+//   const nativeEvent = eventsMap[event];
+//   if (!nativeEvent) {
+//     throw new Error('Invalid event');
+//   }
+//   DeviceEventEmitter.removeAllListeners(nativeEvent);
+//   return DeviceEventEmitter.addListener(nativeEvent, callback);
+// }
 
-module.exports = Notification;
+// //module.exports = Notification;
 
 
 export default class App extends Component {
 
   render() {
     return <Navigator />
+
+    // return(
+    //   <View>Happy</View>
+    //  )
   }
 }
+
+//AppRegistry.registerComponent('App', () => App);
--- notificationApp/screens/AllNotifications.js
+++ notificationApp/screens/AllNotifications.js
@@ -1,11 +1,82 @@
 import React, { Component } from 'react'
-import { Text, View, TouchableHighlight } from 'react-native'
+import { Text, View, TouchableHighlight, PermissionsAndroid } from 'react-native'
+import Notification from "../modules/Notification"
+
+const PERMISSION = PermissionsAndroid.PERMISSIONS.READ_PHONE_STATE;
 
 export default class AllNotifications extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+          sender: '',
+          text: '',
+          apps: [],
+        };
+      }
+    
 
     static navigationOptions = {
         title: 'AllNotifications',
     };
+
+    componentDidMount() {
+        this._checkPermissionPhoneState();
+        this._checkPermissionNotification();
+    
+        Notification.on('notification', (data) => {
+          //console.log("AppState", AppState.currentState);
+          console.log("notification received", JSON.stringify(data));
+          if (AppState.currentState === 'active') {
+            this.setState({
+              sender: data.app,
+              text: data.text
+            });
+          }
+        });
+      }
+    
+      _checkPermissionPhoneState = () => {
+        PermissionsAndroid.check(PERMISSION).then((result) => {
+          if (!result) {
+            PermissionsAndroid.requestPermission(PERMISSION).then((result) => {
+              if (result) {
+                console.log("User accepted read phone state permission");
+              } else {
+                console.log("User refused read phone state permission");
+              }
+            });
+          }
+        });
+      }
+    
+      _checkPermissionNotification = () => {
+        Notification.getPermissionStatus()
+          .then(response => {
+            if (response === 'denied') {
+              this._requestPermissionNotification();
+            }
+          });
+      }
+    
+      _requestPermissionNotification = () => {
+        Alert.alert(
+          'Can we access your notifications?',
+          'We need access so that blah blah blah',
+          [
+            {text: 'No', onPress: () => console.log('permission denied')},
+            {text: 'OK', onPress: Notification.requestPermission},
+          ]
+        )
+      }
+      
+      getInstalledApps = () => {
+        Notification.getInstalledApps()
+          .then(response => {
+            console.log("Installed Apps", JSON.stringify(response));
+            this.setState({apps: response});
+          });
+      }    
 
     render() {
         return (
